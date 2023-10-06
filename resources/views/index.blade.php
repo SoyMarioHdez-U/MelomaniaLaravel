1a5457730d02adb80d5175c65b64f44c844e1204
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2023-10-06 00:11:44 -0600

    segundo commit con mejoras en enlaces

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,13 @@
         <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="http://localhost/MelomaniaSV/Img/inicio/1.png" class="d-block w-100" alt="...">
+                    <img src="{{ asset('images/inicio/1.png') }}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="http://localhost/MelomaniaSV/Img/inicio/2.png" class="d-block w-100" alt="...">
+                    <img src="{{ asset('images/inicio/2.png') }}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="http://localhost/MelomaniaSV/Img/inicio/3.png" class="d-block w-100" alt="...">
+                    <img src="{{ asset('images/inicio/3.png') }}" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
