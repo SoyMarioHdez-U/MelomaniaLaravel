ac4b48338eaa95a96a8f14c24d345914ef204a30
alerodriguez23 <jenniffer.rodriguez@catolica.edu.sv>
2023-10-06 23:59:19 -0600

    lecciones

--- resources/views/leccionestrompeta.php
+++ resources/views/leccionestrompeta.php
@@ -1,26 +1,26 @@
 @extends('plantilla')
-@section('content')
-<div class="leccion">
+@yield('content')
+ <div class="leccion">
     <h1>Lección 1</h1>
-    <p>Contenido de la lección...</p>
-</div>
+    <p><iframe width="560" height="315" src="https://www.youtube.com/embed/_VkDa3ADjuQ?si=UZlCtWNHP9hChtTt" frameborder="0" allowfullscreen></iframe></p>
+ </div>
 
-<div class="leccion">
+ <div class="leccion">
     <h1>Lección 2</h1>
-    <p>Contenido de la lección...</p>
-</div>
+    <p><iframe width="560" height="315" src="https://www.youtube.com/embed/ICyGgIbnUx0?si=2Lc0Z-hyF7Rpgv7v" frameborder="0" allowfullscreen></iframe></p>
+ </div>
 
-<div class="leccion">
+ <div class="leccion">
     <h1>Lección 3</h1>
-    <p>Contenido de la lección...</p>
-</div>
+    <p><iframe width="560" height="315" src="https://www.youtube.com/embed/x8PmMbAvSU4?si=LQ_-3ZWqOKMkE4p6"  frameborder="0" allowfullscreen></iframe></p>
+ </div>
 
-<div class="marcador">
+ <div class="marcador">
     <p>Progreso: <span id="progreso">0%</span></p>
-</div>
+ </div>
 
-<button id="avanzar">Siguiente</button>
+ <button id="avanzar">Siguiente</button>
 
-<script src="http://localhost//MelomaniaSV/js/cursos.js"></script>
-    
+ <script src="http://localhost//MelomaniaSV/js/cursos.js"></script>
+
 @endsection
