d607f82d004a886b49b742621cafffb623f63705
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2023-10-06 21:53:46 -0600

    Tienda con todos los productos

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -73,7 +73,7 @@
 
             <div class="col">
               <div class="card shadow-sm">
-              <img src="{{ asset('images/productos/4/guit-acus.jpeg') }}" alt="" width="259" height="200">
+              <img src="{{ asset('images/productos/4/yamaha-sbp2f5.jpg') }}" alt="" width="259" height="200">
                 <div class="card-body">
                   <h5 class="card-title">BATERIA ACUSTICA PROFESIONAL YAMAHA STAGE CUSTOM</h5>
                   <p class="card-text">$4,000.00 </p>
@@ -89,7 +89,7 @@
 
             <div class="col">
               <div class="card shadow-sm">
-              <img src="{{ asset('images/productos/5/guit-acus.jpeg') }}" alt="" width="259" height="200">
+              <img src="{{ asset('images/productos/5/lpa1331-1-1030x1030.jpg') }}" alt="" width="259" height="200">
                 <div class="card-body">
                   <h5 class="card-title">CAJON PERUANO LP ASPIRE 1331 NATURAL</h5>
                   <p class="card-text">$120.00 </p>
@@ -105,7 +105,7 @@
 
             <div class="col">
               <div class="card shadow-sm">
-              <img src="{{ asset('images/productos/6/guit-acus.jpeg') }}" alt="" width="259" height="200">
+              <img src="{{ asset('images/productos/6/jackson-js32rt-dinky-151963.jpg') }}" alt="" width="259" height="200">
                 <div class="card-body">
                   <h5 class="card-title">GUITARRA ELECTRICA JS32RT JACKSON</h5>
                   <p class="card-text">$1000.00 </p>
@@ -118,6 +118,103 @@
                 </div>
               </div>
             </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+              <img src="{{ asset('images/productos/7/psre273.jpg') }}" alt="" width="259" height="200">
+                <div class="card-body">
+                  <h5 class="card-title">TECLADO ELECTRICO PSR-E273</h5>
+                  <p class="card-text">$299.00 </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href=" " class="botondetalles">Detalles</a>
+                    </div>
+                      <a href=" " class="botoncarrito">Agregar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+              <img src="{{ asset('images/productos/8/yamaha-pss-a50-teclado-portatil.jpg') }}" alt="" width="259" height="200">
+                <div class="card-body">
+                  <h5 class="card-title">TECLADO PORTATIL PSS-A50 YAMAHA</h5>
+                  <p class="card-text">$16.00 </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href=" " class="botondetalles">Detalles</a>
+                    </div>
+                      <a href=" " class="botoncarrito">Agregar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+              <img src="{{ asset('images/productos/9/xps-30_gal.jpg') }}" alt="" width="259" height="200">
+                <div class="card-body">
+                  <h5 class="card-title">TECLADO ROLAND XPS-30</h5>
+                  <p class="card-text">$5000.00 </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href=" " class="botondetalles">Detalles</a>
+                    </div>
+                      <a href=" " class="botoncarrito">Agregar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+              <img src="{{ asset('images/productos/10/yas-62-1030x1030.jpg') }}" alt="" width="259" height="200">
+                <div class="card-body">
+                  <h5 class="card-title">SAXOFON ALTO DORADO YAS-62 YAMAHA</h5>
+                  <p class="card-text">$600.00 </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href=" " class="botondetalles">Detalles</a>
+                    </div>
+                      <a href=" " class="botoncarrito">Agregar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+              <img src="{{ asset('images/productos/11/YTR2330YA-1030x1030.jpg') }}" alt="" width="259" height="200">
+                <div class="card-body">
+                  <h5 class="card-title">TROMPETA DORADA SERIE STANDAR YTR2330 YAMAHA</h5>
+                  <p class="card-text">$400.00 </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href=" " class="botondetalles">Detalles</a>
+                    </div>
+                      <a href=" " class="botoncarrito">Agregar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+              <img src="{{ asset('images/productos/12/S082-3-4.jpg') }}" alt="" width="259" height="200">
+                <div class="card-body">
+                  <h5 class="card-title">VIOLIN 3/4 XCALIBUR</h5>
+                  <p class="card-text">$69.00 </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href=" " class="botondetalles">Detalles</a>
+                    </div>
+                      <a href=" " class="botoncarrito">Agregar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 @endsection
